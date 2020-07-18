9832d271b34a691e3c5b1ffe312a87713d9d8a3d
VicLo <victoria2666@gmail.com>
2020-07-17 20:24:39 -0400

    第四好了，一点困了

--- controllers/tea.js
+++ controllers/tea.js
@@ -1,11 +1,39 @@
-const newTea = (req, res, next) => {
-    res.json({message: "POST new tea"});
-};
-
+//GET teas
 const getAllTea = (req, res, next) => {
     res.json({message: "GET all tea"});
 };
 
+//POST teas
+const newTea = (req, res, next) => {
+    res.json({message: "POST new tea"});
+};
 
+//DELETE teas
+const deleteAllTea = (req, res, next) => {
+    res.json({message: "DELETE all tea"});
+};
 
-module.exports = {newTea, getAllTea};
+//GET 1 tea
+const getOneTea = (req, res, next) => {
+    res.json({message: "GET 1 tea"});
+};
+
+//POST 1 tea comment
+const newComment = (req, res, next) => {
+    res.json({message: "POST 1 tea comment"});
+};
+
+//DELETE 1 tea
+const deleteOneTea = (req, res, next) => {
+    res.json({message: "DELETE 1 tea"});
+};
+
+//export controller
+module.exports = {
+    getAllTea, 
+    newTea,
+    deleteAllTea,
+    getOneTea,
+    newComment,
+    deleteOneTea
+};
--- routes/tea.js
+++ routes/tea.js
@@ -5,5 +5,10 @@ const teaController = require('../controllers/tea');
 
 router.get('/tea', teaController.getAllTea);
 router.post('/tea', teaController.newTea);
+router.delete('/tea', teaController.deleteAllTea);
+
+router.get('/tea/:id', teaController.getOneTea);
+router.post('/tea/:id', teaController.newComment);
+router.delete('/tea/:id', teaController.deleteOneTea);
 
 module.exports = router;
